ae2953c7c3dd9b7bb88a140e8b416122d8f95341
Craig Shaw <craig@craigshaw.com>
2016-01-21 20:27:07 +0000

    Added Windows service project

---
+++ SoldOutSearchMonkey/Service/SearchMonkeyService.cs
@@ -0,0 +1,29 @@
+﻿using log4net;
+using SoldOutBusiness.Repository;
+using System;
+
+namespace SoldOutSearchMonkey.Service
+{
+    public class SearchMonkeyService
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(SearchMonkeyService));
+        //(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public void Start()
+        {
+            var repo = new SearchRepository();
+
+            var searches = repo.GetAllSearches();
+
+            foreach (var search in searches)
+            {
+                _log.Info(search.Name);
+            }           
+        }
+
+        public void Stop()
+        {
+            _log.Info("Stopped");
+        }
+    }
+}
---
+++ SoldOutSearchMonkey/SoldOutSearchMonkey.cs
@@ -0,0 +1,40 @@
+﻿using SoldOutSearchMonkey.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace SoldOutSearchMonkey
+{
+    class SoldOutSearchMonkey
+    {
+        private const string ServiceDescription = "SoldOut worker service. Scrapes auction results from eBay and logs them to a data store";
+        private const string ServiceDisplayName = "SoldOut Search Monkey";
+        private const string ServiceName = "SoldOutSearchMonkey";
+
+        static void Main(string[] args)
+        {
+            new SoldOutSearchMonkey().Startup();
+        }
+
+        private void Startup()
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<SearchMonkeyService>(s =>
+                {
+                    s.ConstructUsing(name => new SearchMonkeyService());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription(ServiceDescription);
+                x.SetDisplayName(ServiceDisplayName);
+                x.SetServiceName(ServiceName);
+            });
+        }
+    }
+}
