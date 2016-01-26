84345f5fa78774d13e449f58ed070ab6d9cf981d
Craig Shaw <craig@craigshaw.com>
2016-01-26 22:06:12 +0000

    Basic layout / UI components added

--- SoldOut/MainWindow.xaml.cs
+++ SoldOut/MainWindow.xaml.cs
@@ -24,33 +24,54 @@ namespace SoldOut
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private ISearchRepository _repo;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _repo = new SearchRepository();
 
             InitialiseSearchGrid();
         }
 
         private void InitialiseSearchGrid()
         {
-            using (var repo = new SearchRepository())
-            {
-                // Bind data to the search grid
-                Searches.ItemsSource = repo.GetAllSearches().OrderByDescending(s => s.LastCleansed);
+            // Bind data to the search grid
+            Searches.ItemsSource = _repo.GetAllSearches().OrderByDescending(s => s.LastCleansed);
 
-                // Select the first item
-                Searches.SelectedIndex = 0;
-
-                // Now load the results for that search
-                SearchResults.SelectionMode = DataGridSelectionMode.Extended;
-                SearchResults.ItemsSource = repo.GetSearchResults(((Search)Searches.SelectedItem).SearchId).ToList();
-            }
+            // Select the first item
+            Searches.SelectedIndex = 0;
         }
 
         private void HandleListingClick(object sender, RoutedEventArgs e)
         {
             Hyperlink link = (Hyperlink)e.OriginalSource;
             Process.Start(link.NavigateUri.AbsoluteUri);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the selected items
+            var items = SearchResults.SelectedItems;
+
+            // TODO: Now remove them via the repository
+        }
+
+        private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // Clean up the repo
+            if(_repo != null)
+            {
+                _repo.Dispose();
+                _repo = null;
+            }
+        }
+
+        private void Searches_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Now load the results for that search
+            SearchResults.ItemsSource = _repo.GetSearchResults(((Search)Searches.SelectedItem).SearchId).ToList();
         }
     }
 }
