10e8ff2f2b63dafd9ea848cf5734f36f840a7cd7
Craig Shaw <craig@craigshaw.com>
2016-02-01 21:21:36 +0000

    Added Web project

---
+++ SoldOutWeb/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using SoldOutBusiness.Repository;
+using System.Web.Mvc;
+
+namespace SoldOutWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ISearchRepository _repository;
+
+        public HomeController()
+        {
+            _repository = new SearchRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var searches = _repository.GetAllSearches();
+            return View(searches);
+        }
+
+        public ActionResult Search(int id)
+        {
+            var search = _repository.GetSearchByID(id);
+
+            return View(id);
+        }
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+    }
+}
