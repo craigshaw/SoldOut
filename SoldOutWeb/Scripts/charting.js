920f75e2ddde0860d3e1be507f02db714f633bea
Craig Shaw <craig@craigshaw.com>
2016-02-03 20:43:31 +0000

    Extracted chart creation JS from Search view

---
+++ SoldOutWeb/Scripts/charting.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    google.load('visualization', '1', {
+        'packages': ['corechart', 'controls']
+    });
+
+    $(function () {
+        var searchId = $('#chartContainer').attr('data-search-id');
+
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json',
+            url: '/Home/SearchSummary/' + searchId,
+            success: function (chartsdata) {
+
+                // Create the chart data from the API response
+                var data = new google.visualization.DataTable();
+
+                data.addColumn('string', 'Date');
+                data.addColumn('number', 'Price');
+
+                for (var i = 0; i < chartsdata.length; i++) {
+                    data.addRow([chartsdata[i].PricePeriod, chartsdata[i].AveragePrice]);
+                }
+
+                // Create and draw the chart
+                var chart = new google.visualization.LineChart(document.getElementById('chartContainer'));
+
+                chart.draw(data,
+                  {
+                      title: "TODO: Chart Title"
+                  });
+            },
+            error: function () {
+                // TODO: Make this better - show an error message inline or something
+                alert("Couldn't get chart data");
+            }
+        });
+    })
+})();
