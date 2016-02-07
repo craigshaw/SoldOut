6303d2f2d0f3ead23817458a088f18e87f150df7
Craig Shaw <craig@craigshaw.com>
2016-02-07 20:09:48 +0000

    First move towards SQL Server

---
+++ SoldOutBusiness/DAL/SoldOutDbInitialiser.cs
@@ -0,0 +1,24 @@
+﻿using SoldOutBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace SoldOutBusiness.DAL
+{
+    public class SoldOutDbInitialiser : DropCreateDatabaseIfModelChanges<SearchContext>
+    {
+        protected override void Seed(SearchContext context)
+        {
+            base.Seed(context);
+
+            var searches = new List<Search>()
+            {
+                new Search() { Name = "Lego 76001", Description = "The bat vs. Bane", Link = "http://brickset.com/sets/76001-1/The-Bat-vs-Bane-Tumbler-Chase",
+                LastCleansed = DateTime.Now.AddYears(-1), LastRun = DateTime.Now.AddYears(-1)}
+            };
+
+            context.Searches.AddRange(searches);
+            context.SaveChanges();
+        }
+    }
+}
