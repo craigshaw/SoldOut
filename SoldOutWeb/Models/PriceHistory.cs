5dc99027a6cb9412c566e2d7097ee15450a44b5c
dewhal <dewhal@gmail.com>
2016-02-26 18:26:22 +0000

    Improvements to SMA and added Exponential Moving Average and started on MACD

--- SoldOutWeb/Models/PriceHistory.cs
+++ SoldOutWeb/Models/PriceHistory.cs
@@ -4,13 +4,6 @@ using System.Collections.Generic;
 
 namespace SoldOutWeb.Models
 {
-    public class PriceSummary
-    {
-        public double Price { get; set; }
-        public DateTime PricePoint { get; set; }
-
-    }
-
     public class PriceHistory
     {
 
@@ -53,21 +46,16 @@ namespace SoldOutWeb.Models
 
             prices = new List<PriceHistory>();
 
-                var allPrices =  from pricepoint in _parent.SearchResults
-                       group pricepoint by new { pricepoint.EndTime.Value.Day, pricepoint.EndTime.Value.Month, pricepoint.EndTime.Value.Year } into grp
-                       orderby grp.Key.Year, grp.Key.Month, grp.Key.Day
-                       
-                       select new PriceHistory()
-                       {
-                           PricePeriod = DateTime.Parse($"{grp.Key.Day}/{grp.Key.Month:D2}/{grp.Key.Year}"),
-                           AveragePrice = (double)grp.Average(pricepoint => pricepoint.Price)
-                       };
+            var allPrices =  from pricepoint in _parent.SearchResults
+                    group pricepoint by new { pricepoint.EndTime.Value.Day, pricepoint.EndTime.Value.Month, pricepoint.EndTime.Value.Year } into grp
+                    orderby grp.Key.Year, grp.Key.Month, grp.Key.Day                       
+                    select new PriceHistory()
+                    {
+                        PricePeriod = DateTime.Parse($"{grp.Key.Day}/{grp.Key.Month:D2}/{grp.Key.Year}"),
+                        AveragePrice = (double)grp.Average(pricepoint => pricepoint.Price)
+                    };
 
-            //where DateTime.Parse($"{grp.Key.Day}/{grp.Key.Month:D2}/{grp.Key.Year}") < this.PricePeriod // Exclude ourselves and any prices after us
-            //&& DateTime.Parse($"{grp.Key.Day}/{grp.Key.Month:D2}/{grp.Key.Year}") >= this.PricePeriod.Subtract(new TimeSpan(0,interval,0))
-
-
-
+            // Create a list of all the prices prior to ourselves that are within the required interval i.e 5 days
             foreach (PriceHistory price in allPrices)
             {
                 if (price.PricePeriod < this.PricePeriod && price.PricePeriod >= this.PricePeriod.Subtract(new TimeSpan(interval,0, 0, 0)))
@@ -84,13 +72,15 @@ namespace SoldOutWeb.Models
             return prices;
         }
 
-        private double CalculatePriceSTDDeviation(int interval)
+        private double? CalculatePriceSTDDeviation(int interval)
         {
             double M = 0.0;
             double S = 0.0;
             int k = 0;
 
             IEnumerable<PriceHistory> priorPrices = GetPriorPrices(interval);
+
+            if (priorPrices == null) return null;
 
             foreach (PriceHistory price in priorPrices)
             {
@@ -103,13 +93,21 @@ namespace SoldOutWeb.Models
             return Math.Sqrt(S / (k - 1));
         }
 
-        public double SMA
+        public double? SMA
         {
             get
             {
                 double simpleMovingAverage = 0.00d;
 
+                if (_parent.SearchResults.Count() - 1 < this.Interval) return null;
+
                 IEnumerable<PriceHistory> priorPrices = GetPriorPrices(this.Interval -1); // -1 as we need to add our own price
+
+                // We have no prior prices
+                if (priorPrices == null) return null;
+
+                // We don't have enough prices
+                if (priorPrices.Count() < this.Interval - 1) return null;
 
                 foreach (PriceHistory price in priorPrices)
                 {
@@ -119,6 +117,43 @@ namespace SoldOutWeb.Models
                 simpleMovingAverage += this.AveragePrice; // Add ourselves
 
                 return simpleMovingAverage / this.Interval;
+            }
+        }
+
+        public double? EMA
+        {
+            get
+            {
+                double? exponentialMovingAverage = 0.00d;
+                double multiplier = 0.00d;
+                PriceHistory yesterdaysPrice;
+
+                if (_parent.SearchResults.Count() - 1 < this.Interval) return null;
+
+                multiplier = 2 / this.Interval + 1;
+
+                yesterdaysPrice = (PriceHistory)GetPriorPrices(1);
+
+                exponentialMovingAverage += this.AveragePrice * multiplier + yesterdaysPrice.EMA *(1 - multiplier);
+
+                return exponentialMovingAverage;
+            }
+        }
+
+            public double? MACD
+        {
+            get
+            {
+                double? lowerMACD = 0.00;
+                double? upperMACD = 0.00;
+
+                this.Interval = 12;
+                lowerMACD = this.EMA;
+
+                this.Interval = 26;
+                upperMACD = this.EMA;
+
+                return lowerMACD - upperMACD;
             }
         }
     }
