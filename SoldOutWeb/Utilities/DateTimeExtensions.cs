21a3c75a7b1fafaf0662a04f41c6aceadcc24158
Craig Shaw <craig@craigshaw.com>
2016-02-28 08:11:36 +0000

    Added extension method for getting week number. PR for weeklies.

---
+++ SoldOutWeb/Utilities/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace SoldOutWeb.Utilities
+{
+    static class DateTimeExtensions
+    {
+        private static GregorianCalendar _calendar = new GregorianCalendar();
+
+        public static int GetWeek(this DateTime time)
+        {
+            return _calendar.GetWeekOfYear(time, CalendarWeekRule.FirstDay, DayOfWeek.Sunday);
+        }
+    }
+}
