1b0c4a792bea658672a7e587188e1a85f9ec917c
dewhal <dewhal@gmail.com>
2016-02-29 18:23:42 +0000

    Added DAL and Model objects for Category and Product

---
+++ SoldOutBusiness/DAL/ProductContext.cs
@@ -0,0 +1,26 @@
+﻿using SoldOutBusiness.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace SoldOutBusiness.DAL
+{
+    public class ProductContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Product> RelatedProducts { get; set; }
+
+        public ProductContext()
+        {
+            // Turn off the Migrations, (NOT a code first Db)
+            Database.SetInitializer<ProductContext>(null);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Our database convention does not pluralize table names
+            modelBuilder.Conventions
+                .Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ SoldOutBusiness/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace SoldOutBusiness.Models
+{
+    public class Category
+    {
+        public long CategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public long? ParentCategoryId { get; set; }
+
+    }
+}
