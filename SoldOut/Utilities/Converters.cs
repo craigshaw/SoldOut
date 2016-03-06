a5a1a13572164b7ea1cfaf0eb038523fa08c3c9e
Craig Shaw <craig@craigshaw.com>
2016-03-06 00:07:30 +0000

    Support for new and used items

---
+++ SoldOut/Utilities/Converters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SoldOutCleanser.Utilities
+{
+    internal class ConditionIdConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // TODO: Improve this. Should really use an IConditionResolver
+            if(value is int)
+            {
+                if ((int)value == 2)
+                    return "New";
+                else
+                    return "Used";
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
