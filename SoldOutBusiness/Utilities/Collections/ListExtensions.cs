be86178b39378633dbfab1b760a2af037e12bacf
dewhal <dewhal@gmail.com>
2016-03-09 20:45:38 +0000

    Moved LinqExtensions to ListExtensions where it should have been in the first place!

--- SoldOutBusiness/Utilities/Collections/ListExtensions.cs
+++ SoldOutBusiness/Utilities/Collections/ListExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SoldOutBusiness.Utilities.Collections
 {
@@ -7,6 +8,19 @@ namespace SoldOutBusiness.Utilities.Collections
         public static bool IsNullOrEmpty<T>(this IList<T> source)
         {
             return source == null || source.Count == 0;
+        }
+
+        public static IEnumerable<T> SelectNestedChildren<T>
+           (this IEnumerable<T> source, Func<T, IEnumerable<T>> selector)
+        {
+            foreach (T item in source)
+            {
+                yield return item;
+                foreach (T subItem in SelectNestedChildren(selector(item), selector))
+                {
+                    yield return subItem;
+                }
+            }
         }
     }
 }
